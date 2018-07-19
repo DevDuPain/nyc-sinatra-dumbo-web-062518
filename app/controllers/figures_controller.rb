72ee611b5835a2622bd32ab50d966fd7a38ce884
runandrerun <the.asantiagojr@gmail.com>
2018-07-19 14:13:07 -0400

    filled out controllers for figures

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,3 +1,38 @@
 class FiguresController < ApplicationController
-  
+
+  get '/figures' do
+    erb :'/figures/index'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params)
+    redirect to '/figures'
+  end
+
+  get '/figures/new' do
+    @figures = Figure.all
+    erb :'/figures/new'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'/figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.update(params[:id], figure)
+    redirect to "/figures/#{@figure.id}"
+  end
+
+
+  private
+  def figure
+    params[:figure]
+  end
+
 end
